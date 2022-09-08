f92187e9346bbd9aaf154ee598ecd5da51cf0c46
vladishum <112489778+vladishum@users.noreply.github.com>
2022-09-08 11:18:35 +0100

    Add files via upload

---
+++ HTML and CSS/Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,7 @@
+alert("Hello World! I am an alert box!");
+
+function My_First_Function(){
+				var String = "Kiss me, I am Irish!";
+				var result = String.fontcolor("green");
+				document.getElementById("Irish").innerHTML = result;
+				}	
